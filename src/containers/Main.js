5b128ef0d23fa53061a4bec04414fe2870f0d2f9
Han-Lin-zc <hlin04@gmail.com>
2020-04-09 13:50:35 -0400

    Delete features

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -4,15 +4,9 @@ import Greeting from "./greeting/Greeting";
 import Skills from "./skills/Skills";
 import StackProgress from "./skillProgress/skillProgress";
 import Projects from "./projects/Projects";
-import StartupProject from "./StartupProjects/StartupProject";
-import Achievement from "./achievement/Achievement";
-import Blogs from "./blogs/Blogs";
 import Contact from "./contact/Contact";
 import Footer from "../components/footer/Footer";
-import Talks from "./talks/Talks";
-import Podcast from "./podcast/Podcast";
 import Top from "./topbutton/Top";
-import Twitter from "./twitter-embed/twitter";
 
 export default class Main extends Component {
   render() {
@@ -23,12 +17,6 @@ export default class Main extends Component {
         <Skills />
         <StackProgress />
         <Projects />
-        <StartupProject />
-        <Achievement />
-        <Blogs />
-        <Talks />
-        <Twitter />
-        <Podcast />
         <Contact />
         <Footer />
         <Top />
